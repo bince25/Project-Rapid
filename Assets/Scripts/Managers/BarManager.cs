5ecccade953368679b12bea3d573e8777122e947
Baran İNCE <incebaran25@gmail.com>
2024-01-26 22:41:07 +0300

    Merged Scene

--- Assets/Scripts/Managers/BarManager.cs
+++ Assets/Scripts/Managers/BarManager.cs
@@ -10,6 +10,12 @@ public class BarManager : MonoBehaviour
     public Slider slider;
     public TextMeshProUGUI valueText;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.value = 25;
+        valueText.text = slider.value.ToString("F1");
+    }
     // Update is called once per frame
     void Update()
     {
@@ -18,16 +24,25 @@ public class BarManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Increases the slider value by the given value
+    /// </summary>
     public void IncreaseSliderValue(float value){
         slider.value += value;
         valueText.text = slider.value.ToString("F1");
         CheckSliderValue();
     }
+    /// <summary>
+    /// Decreases the slider value by the given value
+    /// </summary>
     public void DecreaseSliderValue(float value){
         slider.value -= value;
         valueText.text = slider.value.ToString("F1");
         CheckSliderValue();
     }
+    /// <summary>
+    /// Checks if the slider value is greater than or equal to 100 or less than or equal to 0
+    /// </summary>
     void CheckSliderValue(){
         if(slider.value >= 100){
             slider.value = 100;
